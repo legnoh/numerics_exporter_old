6d2edfc70517aa34ce030fbf0123b14062089fc1
Ryoma Kai <legknmj@icloud.com>
2021-08-22 23:02:22 +0900

    add netlify setting & numerics sample json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
 function hello(name: string): string {
     return `Hello, ${name}!`;
-  }
+}
   
-  console.log(hello("World"));
+console.log(hello("World"));
+
+const fs = require('fs');
+
+const dir = "./public";
+const filename = "output.json";
+
+const json = {
+  "postfix": "MyUnits",
+  "color": "green",
+  "data": {
+    "value": 1234
+  }
+};
+
+
+if (!fs.existsSync(dir)){
+  fs.mkdirSync(dir);
+}
+
+fs.writeFileSync( dir + "/" + filename, JSON.stringify(json));
